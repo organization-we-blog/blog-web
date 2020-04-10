51bd550a8a3b033618804b8221869d17a29a61ad
withoutgirlfriend <bestyourself@foxmail.com>
2020-04-10 23:33:16 +0800

    '添加路由'

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home/Home.vue'
+import Summary from "../views/Summary/Summary"
+import Diary from "../views/Diary/Diary"
+import MessageBoard from "../views/MessageBoard/MessageBoard";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: '',
+    redirect: '/home'
+  }, {
+    path: '/home',
     name: 'Home',
     component: Home
+  }, {
+    path: '/summary',
+    name: 'Summary',
+    component: Summary
+  }, {
+    path: '/diary',
+    name: 'Diary',
+    component: Diary
+  }, {
+    path: '/messageBoard',
+    name: 'MessageBoard',
+    component: MessageBoard
   }
 ];
 
