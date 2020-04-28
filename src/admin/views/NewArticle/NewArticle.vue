ff8e1aeea4a03f3d379305ba1eae728496ec9a4b
王明 <50187756+kimi233@users.noreply.github.com>
2020-04-28 20:27:59 +0800

    编辑页添加选项组件

--- src/admin/views/NewArticle/NewArticle.vue
+++ src/admin/views/NewArticle/NewArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="width: 100%;">
-    <div class="editBox" style="display:inline-block;height: 1000px;text-align: left;padding: 0 5%">
+    <div class="editBox" style="display:inline-block;text-align: left;"><!--右侧区域-->
       <h1 style="font-size: 30px">添加文章</h1>
       <el-input v-model="formData.title" placeholder="请输入标题"></el-input>
       <Editor
@@ -9,17 +9,48 @@
         height="500px">
       </Editor>
     </div>
-    <div class="editConfigBox" style="display:inline-block;height: 1000px;text-align: left;padding-top: 80px;max-width: 500px">
-      <div style="width:100%;max-width: 500px;">
+    <div class="editConfigBox" style="display:inline-block;text-align: left;padding-top: 80px;"><!--左侧区域-->
+      <OptionItem
+        title="缩略图"
+        class="optionItem"
+      >
         <el-upload
+          style="text-align: center;"
           class="avatar-uploader"
-          action="https://jsonplaceholder.typicode.com/posts/"
+          action="/api/article/uploadImg"
           :show-file-list="false"
+          :multiple="false"
+          name="file"
           :on-success="handleAvatarSuccess"
+          :on-error="handleAvatarError"
           :before-upload="beforeAvatarUpload">
           <img v-if="imageUrl" :src="imageUrl" class="avatar">
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
+      </OptionItem>
+      <OptionItem
+        title="文章简介"
+        class="optionItem">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 4, maxRows: 10}"
+          placeholder="请输入简介"
+          v-model="jj">
+        </el-input>
+      </OptionItem>
+      <OptionItem
+        title="文章分类"
+        class="optionItem">
+
+      </OptionItem>
+      <OptionItem
+        title="文章标签"
+        class="optionItem">
+
+      </OptionItem>
+      <!--<div style="width:100%;max-width: 500px;">
+        <h1>缩略图：</h1>
+
       </div>
       <div style="width:100%;max-width: 500px;">
         <el-row>
@@ -47,21 +78,24 @@
           <el-checkbox label="复选框 E" style="color: #afffb0"></el-checkbox>
           <el-checkbox label="复选框 F" style="color: #ff71c7"></el-checkbox>
           <el-checkbox label="复选框 G" style="color: #ff706f"></el-checkbox>
-      </div>
+      </div>-->
     </div>
   </div>
 </template>
 
 <script>
+  import OptionItem from "./components/OptionItem";
   import Editor from "../../components/Editor/Editor";
   export default {
     name: "NewArticle",
-    components:{Editor},
+    components:{Editor, OptionItem},
     data(){
       return{
+        jj:"",
         imageUrl: '',
         content: localStorage.getItem("editorContentText") || "请开始编辑markdown,点击保存按钮可以避免刷新数据丢失哦",
-        classOptions: [{
+        classOptions: [
+          {
           value: '选项1',
           label: '黄金糕'
         }, {
@@ -86,19 +120,23 @@
     },
     methods:{
       handleAvatarSuccess(res, file) {
+        this.$message.success('图片上传成功');
         this.imageUrl = URL.createObjectURL(file.raw);
       },
+      handleAvatarError() {
+        this.$message.error('图片上传失败');
+      },
       beforeAvatarUpload(file) {
-        const isJPG = file.type === 'image/jpeg';
-        const isLt2M = file.size / 1024 / 1024 < 2;
+        const isJPG = file.type === 'image/jpeg' || file.type === 'image/png';
+        const isLt1M = file.size / 1024 / 1024 < 1;
 
         if (!isJPG) {
-          this.$message.error('上传缩略图片只能是 JPG 格式!');
+          this.$message.error('上传缩略图片只能是 JPG、PNG 格式!');
         }
-        if (!isLt2M) {
+        if (!isLt1M) {
           this.$message.error('上传缩略图大小不能超过 2MB!');
         }
-        return isJPG && isLt2M;
+        return isJPG && isLt1M;
       },
       saveContentText(text/*,html*/){
         //console.log(text);//md内容
@@ -114,10 +152,13 @@
 <style scoped>
   @media screen and (min-width: 1400px){
     .editBox{
-      width: 80%;
+      width: 70%;
     }
     .editConfigBox{
-      width: 20%;
+      width: 30%;
+    }
+    .optionItem{
+      margin: 0 0 15px 40px;
     }
   }
   @media screen and (max-width: 1399px){
@@ -126,6 +167,9 @@
     }
     .editConfigBox{
       width: 100%;
+    }
+    .optionItem{
+      margin: 0 0 40px 25px;
     }
   }
   .avatar-uploader .el-upload {
---
+++ src/admin/views/NewArticle/components/OptionItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="box">
+    <div class="itemHeader"
+         @click="showContent = !showContent">
+      <h1 class="itemTitle">
+        {{title}}
+        <span
+          :style="showContent?'transform: rotate(0deg);':'transform: rotate(180deg);'"
+          class="el-icon-caret-top showIcon">
+        </span>
+      </h1>
+    </div>
+    <el-collapse-transition>
+      <div v-show="showContent">
+        <slot>
+          <div style="height: 50px;">
+            <h1 style="text-align: center;line-height: 50px">空空如也</h1>
+          </div>
+        </slot>
+      </div>
+    </el-collapse-transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "OptionItem",
+    data(){
+      return{
+        showContent: true
+      }
+    },
+    props: {
+      title: {
+        type: String,
+        default: "空"
+      },
+    },
+    methods:{
+    }
+  }
+</script>
+
+<style scoped>
+  .box{
+    display: inline-block;
+    width: 100%;
+    max-width: 300px;
+    border: 1px #eeeeee solid;
+  }
+  .itemHeader{
+    padding: 0 20px;
+    width: 100%;
+    min-height: 35px;
+    background-color: #eeeeee;
+    user-select: none;
+  }
+  .itemHeader>h1{
+    line-height: 35px;
+  }
+  .showIcon{
+    float: right;
+    line-height: 35px;
+    width: 50px;
+    transition: all 0.3s;
+    text-align: center;
+  }
+</style>
